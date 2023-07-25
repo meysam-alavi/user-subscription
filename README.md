65e9f2bc3152f26bae59690bef790b70aadfe791
meysam <meysam.alavi1990@gmail.com>
2023-07-26 00:35:19 +0330

    user subscription app, step 2 [stuff some test]

--- README.md
+++ README.md
@@ -11,11 +11,11 @@
 - php artisan cache:clear
 - php artisan schedule:list
 - php artisan schedule:work
-
 - php artisan app:alert-subscribe-expiration-to-user
-
 - php artisan test
 
+### outbound of containers
+- ./vendor/bin/sail test
 
 - phpmyadmin -> http://localhost:8080
 - laravel mail pit dashboard -> http://localhost:8025/
--- tests/Feature/ApiUserSubscriptionTest.php
+++ tests/Feature/ApiUserSubscriptionTest.php
@@ -8,13 +8,19 @@ use Tests\TestCase;
 
 class ApiUserSubscriptionTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    public function test_register_route(): void
     {
-        $response = $this->get('/');
+        $params = ['name' => 'Meysam', 'email' => 'meysam.alavi@gmail.com', 'password' => '12345600'];
+        $response = $this->post('/api/user/register', $params);
 
         $response->assertStatus(200);
+    }
+
+    public function test_register_route_method(): void
+    {
+        $params = ['name' => 'Meysam', 'email' => 'meysam.alavi@gmail.com', 'password' => '12345600'];
+        $response = $this->get('/api/user/register', $params);
+
+        $response->assertNotFound();
     }
 }
